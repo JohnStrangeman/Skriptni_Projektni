f50b84375a4534ca163fdd70a69f3fdf22f4def2
JohnStrangeman <46049453+JohnStrangeman@users.noreply.github.com>
2025-01-12 16:08:47 +0100

    AAAAAAAAAAAAAAAAAAAAA

--- Placements/script.js
+++ Placements/script.js
@@ -8,20 +8,22 @@ class Placement {
     }
 }
 
-let Placements = [];
+var Placements = [];
+var currTable = [];
 
 function LoadPlayers() {
     fetch('./Placements.json')
     .then((response) => response.json())
     .then((json) => {
         json.forEach((placement) => {
-            Players.push(new Placement(placement.Player, placement.Rank, placement.Tournament, placement.FieldType, placement.Year));
+            Placements.push(new Placement(placement.Player, placement.Rank, placement.Tournament, placement.FieldType, placement.Year));
         });
         PopulateTable(Placements);
     });
 }
 
 function PopulateTable(listPlayers) {
+    currTable = listPlayers;
     let table = document.getElementById("player_ranking");
 
     if(table.childElementCount > 1) {
@@ -75,16 +77,158 @@ function CreateRows(placement, n) {
 }
 
 function SearchPlayers() {
-    let search = document.getElementById("name").value;
+    let player = document.getElementById("Players").value;
+    let tournament = document.getElementById("Tournament").value;
+    let fieldType = document.getElementById("FieldType").value;
+    let year = document.getElementById("Year").value;
     let results = [];
-    Placements.forEach(placement => {
-        if(search.toLowerCase() == placement)
-        {
-            results.push(placement);
-        }
-    });
+
+    results = GetPlacementsBy('player', player, Placements)
+    results = GetPlacementsBy('tournament', tournament, results)
+    results = GetPlacementsBy('field', fieldType, results)
+    results = GetPlacementsBy('year', year, results)
 
     PopulateTable(results);
+}
+/*
+function rankCompare(a, b) {
+    let rankA = a.Rank();
+    let rankB = b.Rank();
+
+    let scoreA = 0;
+    let scoreB = 0;
+
+    switch(rankA) {
+        case 'W':
+            scoreA = 1;
+            break;
+        case 'F':
+            scoreA = 2;
+            break;
+        case 'SF':
+            scoreA = 4;
+            break;
+        case 'QF':
+            scoreA = 8;
+            break;
+        case 'R16':
+            scoreA = 16;
+            break;
+        case 'R32':
+            scoreA = 32;
+            break;
+        case 'R64':
+            scoreA = 64;
+            break;
+        case 'R128':
+            scoreA = 128;
+            break;
+        case '-':
+            scoreA = 200;
+            break;
+        default:
+            scoreA = 0;
+            break;
+    }
+
+    switch(rankB) {
+        case 'W':
+            scoreB = 1;
+            break;
+        case 'F':
+            scoreB = 2;
+            break;
+        case 'SF':
+            scoreB = 4;
+            break;
+        case 'QF':
+            scoreB = 8;
+            break;
+        case 'R16':
+            scoreB = 16;
+            break;
+        case 'R32':
+            scoreB = 32;
+            break;
+        case 'R64':
+            scoreB = 64;
+            break;
+        case 'R128':
+            scoreB = 128;
+            break;
+        case '-':
+            scoreB = 200;
+            break;
+        default:
+            scoreB = 0;
+            break;
+    }
+    return scoreA - scoreB;
+}
+
+function SortRankings() {
+    currTable.sort(rankCompare);
+    alert("Sorted");
+    PopulateTable(currTable);
+}
+
+function yearCompare(a, b) {
+    let yearA = a.Year();
+    let yearB = b.Year();    
+
+    yearA.replace('.', '');
+    yearB.replace('.', '');
+
+    return yearB - yearA;
+}
+
+function SortYear() {
+    currTable.sort(yearCompare);
+    alert("Sorted");    
+    PopulateTable(currTable);
+}
+*/
+function GetPlacementsBy(category, search, placements) {
+    if (search == '-') {
+        return placements;
+    }
+
+    let temp = [];
+
+    switch (category) {
+        case 'player':
+            placements.forEach(placement => {
+                if(placement.Player == search) {
+                    temp.push(placement);
+                }
+            });
+            break;
+        case 'tournament':
+            placements.forEach(placement => {
+                if(placement.Tournament == search) {
+                    temp.push(placement);
+                }
+            });
+            break;
+        case 'field':
+            placements.forEach(placement => {
+                if(placement.FieldType == search) {
+                    temp.push(placement);
+                }
+            });
+            break;
+       case 'year':
+        placements.forEach(placement => {
+            if(placement.Year == search) {
+                temp.push(placement);
+            }
+        });
+        break; 
+        default: 
+            temp = placements
+            break;
+    }
+    return temp;
 }
 
 function RowOpacityIn(id) {
